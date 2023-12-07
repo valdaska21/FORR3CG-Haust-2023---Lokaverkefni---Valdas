e1ccc0da93c14042f48b617966ffb68ded510282
Valdas Kaubrys <89401307+valdaska21@users.noreply.github.com>
2023-12-07 13:18:28 +0000

    Create Batsferd.cpp
    **Dagbókarskrá fyrir Batsferd Aðgerðir**
    
    - **Smiðir:**
      - `Batsferd::Batsferd()` - Sjálfgefinn smiður sem byrjar með sjálfgefnum gildum fyrir `yfirbyggdur`.
      - `Batsferd::Batsferd(int id, int fjoldibokad, int hamarksfjoldi, bool yfirbyggdur)` - Skilgreindur smiður sem stillir gildi með sérstökum hætti, með kalli á `Ferd` smið.
    
    - **Aðgangs- og Stillingaraðferðir:**
      - `getYfirbyggdur()`, `setYfirbyggdur(bool nytt_boolvalue)` - Aðgangs- og stillingaraðferðir fyrir `yfirbyggdur`.
    
    - **Prenta Aðferð:**
      - `Batsferd::prenta()` - Aðferð sem sýnir bátsferðarupplýsingar með `std::cout`, þar sem `ID`, `fjoldibokad`, `hamarksfjoldi` og `yfirbyggdur` eru birtar.
    
    Þessi skrá skráir virkni hvers og eins aðgerðar í `Batsferd` klössnum, sem stjórnar bátsferðaupplýsingum og hefur sérstaka aðferð til að birta þær.

---
+++ Kodinn Hlutaskil/Batsferd.cpp
@@ -0,0 +1,24 @@
+#include "Batsferd.hpp"
+
+Batsferd::Batsferd(){
+    this->yfirbyggdur = false;
+}
+
+Batsferd::Batsferd(int id, int fjoldibokad, int hamarksfjoldi, bool yfirbyggdur) : Ferd(id, fjoldibokad, hamarksfjoldi) {
+    this->yfirbyggdur = yfirbyggdur;
+}
+
+bool Batsferd::getYfirbyggdur(){
+    return this->yfirbyggdur;
+}
+
+void Batsferd::setYfirbyggdur(bool nytt_boolvalue){
+    this->yfirbyggdur = nytt_boolvalue;
+}
+
+void Batsferd::prenta(){
+    std::cout << "Bátsferð| ID: " << this->getId() 
+              << ", Fjöldi Bókað: " << this->getFjoldibokad() 
+              << ", Hámarksfjoldi: " << this->getHamarksfjoldi() 
+              << ", Yfirbyggður: " << this->yfirbyggdur << std::endl;
+}
