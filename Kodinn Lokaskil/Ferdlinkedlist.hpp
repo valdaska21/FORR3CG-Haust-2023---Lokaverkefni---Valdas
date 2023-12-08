fc941accf998a80ce85a718c81e13c3b35cbe75f
Valdas Kaubrys <89401307+valdaska21@users.noreply.github.com>
2023-12-08 10:06:26 +0000

    Update Ferdlinkedlist.hpp
    **Dagbókarskrá fyrir Ferdlinkedlist Klösuna**
    
    - **Klösuskilgreining:**
      - `Ferdlinkedlist` klösin búið til til að stjórna tengdum listum af mismunandi ferðum (`Batsferd`, `Flugferd`, `Hjolaferd`) í safninu.
    
    - **Gagnataka:**
      - `head` - Bendir á fyrsta hnútinn í tengda lista.
    
    - **Aðgerðir:**
      - `Ferdlinkedlist::Ferdlinkedlist()` - Sjálfgefinn smiður sem byrjar með tómum lista.
      - `skraRadad(Ferd* ferdin)` - Aðferð til að skrá ferð í lista í staðsetningu í listanum.
      - `skraBatsferd(int id, int fjoldibokad, int hamarksfjoldi, bool yfirbyggdur)` - Aðferð til að skrá nýja bátsferð í lista.
      - `skraFlugferd(int id, int fjoldibokad, int hamarksfjoldi, std::string hvert)` - Aðferð til að skrá nýja flugferð í lista.
      - `skraHjolaferd(int id, int fjoldibokad, int hamarksfjoldi, int lengdklst)` - Aðferð til að skrá nýja hjólaferð í lista.
      - `prentaAllt()` - Aðferð til að prenta allar ferðir í listanum.
      - `breytaFjoldibokad(int id, int nytt_fjoldibokad)` - Aðferð til að breyta fjölda bókaderra farþega í tiltekinni ferð.
      - `eydaAkvednum(int id)` - Aðferð til að eyða tiltekinni ferð út úr listanum.
      - `skodaAkvedinn(int id)` - Aðferð til að athuga hvort tiltekinn ferð sé í listanum eða ekki.
      - `~Ferdlinkedlist()` - Aðferð til að eyða öllum hnútum í listanum þegar klósin er eytt.
    
    Þessi klasi stýrir ferðalistanum og hefur aðferðir til að skrá, breyta, eyða og skoða upplýsingar um mismunandi ferðir í tengdum listanum. Klósin sér einnig um að sjá um að eyða minnisplássi þegar hann er eytt.

--- Kodinn Lokaskil/Ferdlinkedlist.hpp
+++ Kodinn Lokaskil/Ferdlinkedlist.hpp
@@ -1,1 +1,24 @@
+#pragma once
 
+#include <string>
+#include "FerdNode.hpp"
+#include "Ferd.hpp"
+#include "Batsferd.hpp"
+#include "Flugferd.hpp"
+#include "Hjolaferd.hpp"
+
+class Ferdlinkedlist {
+    private:
+        FerdNode* head;
+    public:
+        Ferdlinkedlist();
+        void skraRadad(Ferd* ferdin);
+        void skraBatsferd(int id, int fjoldibokad, int hamarksfjoldi, bool yfirbyggdur);
+        void skraFlugferd(int id, int fjoldibokad, int hamarksfjoldi, std::string hvert);
+        void skraHjolaferd(int id, int fjoldibokad, int hamarksfjoldi, int lengdklst);
+        void prentaAllt();
+        void breytaFjoldibokad(int id, int nytt_fjoldibokad);
+        void eydaAkvednum(int id);
+        int skodaAkvedinn(int id);
+        ~Ferdlinkedlist();
+};
